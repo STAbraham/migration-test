c72ef6f59fe38cd9d6f8b3b583628ec0a3bfb9c2
Steve Abraham <steve.abraham@gmail.com>
2016-03-31 17:22:57 -0700

    [refactor] (models.py migrations/) moved two classes from migration app to testapp2

--- migration/models.py
+++ migration/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 
-class University(models.Model):
-    university_name = models.CharField(max_length=40)
-    university_address = models.CharField(max_length=40)
-    university_telephone_number = models.BigIntegerField(null=True)
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=50, default='')
     telephone_number = models.BigIntegerField(null=True)
 
-class Class_Directory(models.Model):
-    person = models.ForeignKey(Person, null=True)
-
 class Friends(models.Model):
     person = models.ForeignKey(Person, related_name="person")
     friend = models.ForeignKey(Person, related_name="friend")
+
+class NewDB(models.Model):
+    adhagh = models.CharField(max_length=30)
+    friends = models.ForeignKey(Friends)
--- testapp2/models.py
+++ testapp2/models.py
@@ -2,11 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from migration.models import Person
+
 # Create your models here.
 class Test1(models.Model):
   blahblah = models.CharField(max_length=235)
   tatata = models.CharField(max_length=123)
 
-class Test2(models.Model):
-  asfasdf = models.CharField(max_length=1, default='')
+class Test3(models.Model):
+    link_to_people = models.ForeignKey(Person)
+    trait = models.CharField(max_length=40)
+    traitnum = models.BigIntegerField(null=True)
 
+class University(models.Model):
+    university_name = models.CharField(max_length=40)
+    university_address = models.CharField(max_length=40)
+    university_telephone_number = models.BigIntegerField(null=True)
+
+class Class_Directory(models.Model):
+    person = models.ForeignKey(Person, null=True)
